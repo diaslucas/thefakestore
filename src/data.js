8752ff3cea3209eb6283ed106a57082b719f82b6
diaslucas <diaslucas>
2019-03-22 10:37:18 -0400

    Implementin redux

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const categories = [
+  { name: 'Bathroom', subcategories: ['Sinks', 'Mirrors'] },
+  { name: 'Bedroom', subcategories: ['Beds', 'Matresses', 'Nighstands'] },
+  { name: 'Kitchen', subcategories: ['Chairs', 'Tables'] },
+  { name: 'Living Room', subcategories: ['Sofas', 'Coffee and Side Tables'] },
+  { name: 'Office', subcategories: ['Desks', 'Chairs'] },
+];
+
+export const products = [
+  { name: 'Product 1', categorie: 'Beds', pictures: [] },
+  { name: 'Product 2', categorie: 'Beds', pictures: [] },
+  { name: 'Product 3', categorie: 'Beds', pictures: [] },
+  { name: 'Product 4', categorie: 'Beds', pictures: [] },
+];
