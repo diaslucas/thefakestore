668c57fc2be96945035f5f2c61b35f5a6c9f46da
diaslucas <diaslucas>
2019-03-22 11:03:14 -0400

    Implementing react-redux

--- src/components/CategoriesAside/index.js
+++ src/components/CategoriesAside/index.js
@@ -1,61 +1,72 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { ListGroup, ListGroupItem, UncontrolledCollapse } from 'reactstrap';
 import './categoriesAside.scss';
 
-const CategoriesAside = () => (
-  <aside className="categorie-aside">
-    <ListGroup className="categorie-menu">
-      <ListGroupItem>
-        <div id="toggler1" className="categorie-link">Kitchen</div>
-        <UncontrolledCollapse toggler="#toggler1">
-          <ul>
-            <li>Tables</li>
-            <li>Chairs</li>
-            <li>Microwaves</li>
-          </ul>
-        </UncontrolledCollapse>
-      </ListGroupItem>
-      <ListGroupItem>
-        <div id="toggler2" className="categorie-link">Bedroom</div>
-        <UncontrolledCollapse toggler="#toggler2">
-          <ul>
-            <li>Beds</li>
-            <li>Matresses</li>
-            <li>Nightstands</li>
-          </ul>
-        </UncontrolledCollapse>
-      </ListGroupItem>
-      <ListGroupItem>
-        <div id="toggler3" className="categorie-link">Living Room</div>
-        <UncontrolledCollapse toggler="#toggler3">
-          <ul>
-            <li>Sofas</li>
-            <li>Media Furnitures</li>
-            <li>Coffee and Side Tables</li>
-          </ul>
-        </UncontrolledCollapse>
-      </ListGroupItem>
-      <ListGroupItem>
-        <div id="toggler4" className="categorie-link">Bathroom</div>
-        <UncontrolledCollapse toggler="#toggler4">
-          <ul>
-            <li>Sinks</li>
-            <li>Mirrors</li>
-          </ul>
-        </UncontrolledCollapse>
-      </ListGroupItem>
-      <ListGroupItem>
-        <div id="toggler5" className="categorie-link">Office</div>
-        <UncontrolledCollapse toggler="#toggler5">
-          <ul>
-            <li>Desks</li>
-            <li>Chairs</li>
-            <li>Storages</li>
-          </ul>
-        </UncontrolledCollapse>
-      </ListGroupItem>
-    </ListGroup>
-  </aside>
-);
+const mapStateToProps = state => ({
+  categories: state.categories,
+});
 
-export default CategoriesAside;
+const CategoriesAside = ({ categories }) => {
+  console.log(categories);
+  return (
+    <aside className="categorie-aside">
+      <ListGroup className="categorie-menu">
+        <ListGroupItem>
+          <div id="toggler1" className="categorie-link">Kitchen</div>
+          <UncontrolledCollapse toggler="#toggler1">
+            <ul>
+              <li>Tables</li>
+              <li>Chairs</li>
+              <li>Microwaves</li>
+            </ul>
+          </UncontrolledCollapse>
+        </ListGroupItem>
+        <ListGroupItem>
+          <div id="toggler2" className="categorie-link">Bedroom</div>
+          <UncontrolledCollapse toggler="#toggler2">
+            <ul>
+              <li>Beds</li>
+              <li>Matresses</li>
+              <li>Nightstands</li>
+            </ul>
+          </UncontrolledCollapse>
+        </ListGroupItem>
+        <ListGroupItem>
+          <div id="toggler3" className="categorie-link">Living Room</div>
+          <UncontrolledCollapse toggler="#toggler3">
+            <ul>
+              <li>Sofas</li>
+              <li>Media Furnitures</li>
+              <li>Coffee and Side Tables</li>
+            </ul>
+          </UncontrolledCollapse>
+        </ListGroupItem>
+        <ListGroupItem>
+          <div id="toggler4" className="categorie-link">Bathroom</div>
+          <UncontrolledCollapse toggler="#toggler4">
+            <ul>
+              <li>Sinks</li>
+              <li>Mirrors</li>
+            </ul>
+          </UncontrolledCollapse>
+        </ListGroupItem>
+        <ListGroupItem>
+          <div id="toggler5" className="categorie-link">Office</div>
+          <UncontrolledCollapse toggler="#toggler5">
+            <ul>
+              <li>Desks</li>
+              <li>Chairs</li>
+              <li>Storages</li>
+            </ul>
+          </UncontrolledCollapse>
+        </ListGroupItem>
+      </ListGroup>
+    </aside>
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  null,
+)(CategoriesAside);
