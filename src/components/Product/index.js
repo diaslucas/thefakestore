89f29dc0925103ba24b563f46019b0ef775809d6
diaslucas <diaslucas>
2019-03-25 11:49:48 -0400

    Updating Product component to show received props

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Card, CardImg, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 import './product.scss';
 
 const Product = (props) => {
-  const { name, imgURL } = props;
-
+  const { name, categorie, pictures, price } = props;
+  const firstPic = pictures[0];
   return (
     <Card>
-      <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+      <CardImg top width="100%" src={firstPic} alt={name} />
       <CardBody>
-        <CardTitle>Bed</CardTitle>
-        <CardSubtitle>Color: Black</CardSubtitle>
-        <Button color="success">Add</Button>
+        <CardTitle className="font-weight-bold">{name}</CardTitle>
+        <CardSubtitle className="font-weight-bold">${price}</CardSubtitle>
+        <CardSubtitle>{categorie}</CardSubtitle>
+        <Button color="success">Add to cart</Button>
       </CardBody>
     </Card>
   );
+};
+
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  categorie: PropTypes.string.isRequired,
+  pictures: PropTypes.arrayOf(String).isRequired,
+  price: PropTypes.number.isRequired,
 };
 
 export default Product;
